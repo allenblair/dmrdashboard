3b6d247227b706c866fcbf6aa0dce340dc5fb162
allenblair <allen@allenblair.com>
2019-06-08 20:32:35 -0500

    Added Repeaters and Talkgroups

--- src/Components/repeaters.js
+++ src/Components/repeaters.js
@@ -1,6 +1,19 @@
 const repeaters = [
     {"name":"Huntsville","call":"W4FMX","ID":310112},
-    {"name":"Somerville","call":"WR4JW","ID":310357}
+    {"name":"Somerville","call":"WR4JW","ID":310357},
+    {"name":"Cullman","call":"N4UAI","ID":310127},
+    {"name":"Sheffield","call":"N4GLE","ID":310128},
+    {"name":"Corinth","call":"K5WHB","ID":312804},
+    {"name":"Memphis","call":"W4LET","ID":314706},
+    {"name":"Jonesboro, AR","call":"NZ5E","ID":310514},
+    {"name":"Springfield, MO","call":"K0NXA","ID":312919},
+    {"name":"Boliver, MO","call":"WB0LVR","ID":312924},
+    {"name":"Clinton, MO","call":"W0DR","ID":312936},
+    {"name":"KC, MO","call":"WB0YRG","ID":312906},
+    {"name":"Mission, KS","call":"WB0YRG-1","ID":312010},
+    {"name":"KCI Airport","call":"WB0YRG-2","ID":312932},
+    {"name":"Independence, MO","call":"WB0YRG-3","ID":312907},
+    {"name":"Peculiar, MO","call":"WB0YRG-4","ID":312910},
 ];
 
 export default repeaters;
